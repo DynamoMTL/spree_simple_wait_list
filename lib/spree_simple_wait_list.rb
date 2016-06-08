15c040fdc9eda6e756635a2c2d787185cd65b900
poulping <arnaud@godynamo.com>
2016-06-08 13:54:09 -0400

    adds basic interface to query the wait list

--- lib/spree_simple_wait_list.rb
+++ lib/spree_simple_wait_list.rb
@@ -1,2 +1,15 @@
 require 'spree_core'
 require 'spree_simple_wait_list/engine'
+
+module SimpleWaitList
+  def self.add(spree_user: user, spree_variant: variant)
+    Spree::SimpleWaitList.find_or_create_by(spree_user_id: spree_user.id, variant_id: spree_variant.id)
+  end
+
+  def self.in_stock
+    Spree::SimpleWaitList.pending.in_stock
+  end
+  def self.in_stock_for(user)
+    Spree::SimpleWaitList.for_user(user).pending.in_stock
+  end
+end
