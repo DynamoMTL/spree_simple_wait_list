4917ae4236a9eb0ed893ac576c4704f9ddd4ff2b
poulping <arnaud@godynamo.com>
2016-06-14 14:46:43 -0400

    adds email validation

--- app/models/spree/simple_wait_list.rb
+++ app/models/spree/simple_wait_list.rb
@@ -3,6 +3,8 @@ module Spree
     belongs_to :user, class_name: 'Spree::User', dependent: :destroy
     belongs_to :variant, class_name: 'Spree::Variant', dependent: :destroy
 
+    validates :email, presence: true, format: { with: /\A[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]+\z/ }
+
     scope :pending, -> { where(notified: false) }
 
     def self.for_user(email)
